5cda0be1acf17ecd734118c1326e2df50c07ad4d
altamirus-github <altamirus@gmail.com>
2025-08-11 20:31:20 -0300

    Update register, actors, movies, genres

--- genres/admin.py
+++ genres/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from genres.models import Genre
 
-# Register your models here.
+
+@admin.register(Genre)
+class ActorAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name',)
